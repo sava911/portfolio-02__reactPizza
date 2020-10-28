9335a9acc6da1ef69a8ba2afeb012ac71d2836fc
sava911 <pesss929@gmail.com>
2020-10-28 17:42:22 +0300

    button classes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from "./Header";
+import Button from "./Button";
 
 function App() {
     return (
         <div className="wrapper">
             <Header/>
+            <Button outline >aaaa</Button>
+            <Button>bbbb</Button>
             <div className="content">
                 <div className="container">
                     <div className="content__top">
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import classNames from 'classnames';
 
 
-class Button extends React.Component{
+const Button = (props) => {
+    return (
+        <button className={classNames('button', {
+            'button--outline': props.outline,
+        })}>
+            {props.children}
+        </button>
+    );
+};
 
-}
 
 export default Button;
